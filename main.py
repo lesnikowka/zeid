ed9ee10e86fb9f6117b6fba8ec6b34f3c1ee4741
nik <lesnikowka@mail.ru>
2023-12-23 18:54:56 +0300

    fix zeidel

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-nMax = 100000
+import numpy as np
+
+nMax = 100
 s = 0
-eps = 0.0000001
-eps_max = 0
-eps_cur = 0
+eps = 0.001
+eps_max = 0.
+eps_cur = 0.
 n = 9
-x = [0]*n
-x_old = 0
-x_new = 0
+x = [0.]*n
+x_old = 0.
+x_new = 0.
 
 a = [
     [-832., 16., 0., 400., 0., 0., 0., 0., 0.],
@@ -32,25 +34,34 @@ b = [
     -1014.604
 ]
 
+gauss_sol = np.linalg.solve(a, b)
+
+print("Решение методом Гаусса")
+print(gauss_sol)
+print()
+
 def trueSol(x_, y_):
     return x_**3 + y_**3 + 2
 
+trueSolArr = [trueSol(x_i, y_i) for x_i in [0.25, 0.5, 0.75] for y_i in [0.05, 0.1, 0.15]]
 
+print("Истинное решение: ")
+print(trueSolArr)
+print()
 
 
 while True:
-    eps_max = 0
+    eps_max = 0.
     for i in range(n):
         x_old = x[i]
         x_new = b[i]
         for j in range(n):
             if j != i:
                 x_new -= a[i][j] * x[j]
-            x_new /= a[i][i]
-            eps_cur = abs(x_old - x_new)
-            if eps_cur > eps_max:
-                eps_max = eps_cur
-            x[i] = x_new
+        x_new /= a[i][i]
+        eps_cur = abs(x_old - x_new)
+        eps_max = max(eps_cur, eps_max)
+        x[i] = x_new
     s += 1
     if eps_max <= eps or s >= nMax:
         break
