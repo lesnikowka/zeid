57df91f40d334be5da17948881e1d8a34b571514
nik <lesnikowka@mail.ru>
2023-12-27 16:13:05 +0300

    add grid

--- main3.py
+++ main3.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-nMax = 10000
+nMax = 1000
 s = 0
-eps = 1e-12
+eps = 1e-8
 endCauseIsEps = False
 endCauseIsS = False
 end = False
@@ -11,78 +11,42 @@ A = 0
 B = 1
 C = 0
 D = 0.2
-#M = int(input("Введите число разбиений по y: "))
-#N = int(input("Введите число разбиений по x: "))
+M = 4
+N = 4
+h = (B-A)/N
+k = (D-C)/M
 
-M = 0
-N = 0
+M = int(input("Введите число разбиений по y: "))
+N = int(input("Введите число разбиений по x: "))
 
 def trueSol(x_, y_):
     return x_**3 + y_**3 + 2
 def u(x_, y_):
     return trueSol(x_, y_)
+def u0y(y_):
+    return y_**3 + 2
+def u1y(y_):
+    return y_**3 + 3
+def ux0(x_):
+    return x_**3 + 2
+def ux0_2(x_):
+    return x_**3 + 251./125.
 def f(x_, y_):
     return -6 * (x_ + y_)
 
-def print_v():
-    temp_v = np.array([[0.]*(N+1)]*(M+1))
-    temp_true_sol_arr = np.array([[0.]*(N+1)]*(M+1))
-    for j in range(M,-1,-1):
-        for i in range(0, N + 1):
-            temp_v[M - j, i] = v[i][j]
-            temp_true_sol_arr[M - j, i] = trueSol(i * h, j * k)
-    print("V(N) в виде сетки:")
-    print(temp_v)
-    print("Истинное решение в виде сетки")
-    print(temp_true_sol_arr)
+#Создаем нулевую матрицу V
+v = [ ([0.]*(M+1)).copy() for i in range(N + 1)]
 
 #Заполняем граничные значения
-
-file = open("grid.txt")
-
-v = []
-
-#for j in range(M + 1):
-#    v.append(file.readline().split())
-
-while True:
-    line = file.readline()
-    if not line:
-        break
-    splitted = line.split()
-    N = len(splitted)
-    M += 1
-    v.append(splitted)
-
-N -= 1
-M -= 1
-
-h = (B-A)/N
-k = (D-C)/M
-
-
-new_v = [(['a']*(M+1)).copy() for i in range(N + 1)]
-
 for i in range(M + 1):
-    for j in range(N + 1):
-          new_v[j][M - i] = v[i][j]
-
-v = new_v
-
-print(np.array(v))
+    v[0][i] = u0y(i * k)
+    v[len(v)-1][i] = u1y(i * k)
 
 for i in range(N + 1):
-    for j in range(M + 1):
-        if v[i][j] == '0':
-            v[i][j] = u(i * h, j * k)
-        else:
-            v[i][j] = 0.
-
-print(np.array(v))
+    v[i][0] = ux0(i * h)
+    v[i][len(v[0])-1] = ux0_2(i * h)
 
 AA = -2 * ( 1 / (h ** 2) + 1 / (k ** 2))
-kh = (1 / (h**2))
-kk = (1 / (k**2))
 
 #метод Зейделя
 while not end:
@@ -91,10 +55,10 @@ while not end:
         for i_ in range(1, N):
             v_old = v[i_][j_]
             v[i_][j_] = -f(i_*h, j_*k)
-            v[i_][j_] -= v[i_ - 1][j_] * kh
-            v[i_][j_] -= v[i_ + 1][j_] * kh
-            v[i_][j_] -= v[i_ ][j_ - 1] * kk
-            v[i_][j_] -= v[i_ ][j_ + 1] * kk
+            v[i_][j_] -= v[i_ - 1][j_] * (1 / (h**2))
+            v[i_][j_] -= v[i_ + 1][j_] * (1 / (h**2))
+            v[i_][j_] -= v[i_ ][j_ - 1] * (1 / (k**2))
+            v[i_][j_] -= v[i_ ][j_ + 1] * (1 / (k**2))
             v[i_][j_] /= AA
             eps_cur = abs(v_old - v[i_][j_])
             eps_max = max(eps_cur, eps_max)
@@ -109,10 +73,17 @@ while not end:
         end = True
 
 
+temp_v = np.array([[0.]*(N+1)]*(M+1))
+temp_true_sol_arr = np.array([[0.]*(N+1)]*(M+1))
+for j in range(M,-1,-1):
+    for i in range(0, N + 1):
+        temp_v[M - j, i] = v[i][j]
+        temp_true_sol_arr[M - j, i] = trueSol(i * h, j * k)
 
-
-print_v()
-
+print("V(N) в виде сетки:")
+print(temp_v)
+print("Истинное решение в виде сетки")
+print(temp_true_sol_arr)
 
 # ИНФОРМАЦИЯ ДЛЯ ОТЧЕТА
 
@@ -151,10 +122,6 @@ def getInfo():
 
     ss += "Норма невязки на выходе: " + str(inf_norm_) + "\n"
 
-    residual_ = [abs(r) for r in residual_]
-
-    ss += "Max|U-V|: " + str(max(residual_)) + "\n"
-
     ss += """При решении СЛАУ Ax=b методом Зейделя с критериями остановки Nmax=% и eps = %
      за S=% итераций достигнута точность eps_max=% и получено численное решение:\n\n"""
 
@@ -174,6 +141,10 @@ for j in range(M-1):
         x[j*(N-1)+i] = v[i+1][j+1]
 
 trueSolArr = [trueSol(x_i * h, y_i * k)  for y_i in range(1, M) for x_i in range(1, N)]
+#print("Истинное решение: ")
+#print(trueSolArr)
+#print()
+
 info = getInfo()
 print(info)
 
